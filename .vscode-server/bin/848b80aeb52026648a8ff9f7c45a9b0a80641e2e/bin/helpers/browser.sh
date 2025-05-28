d54be618956691c8d7b03f0f88173adf405d7746
pon <youbuxiaming@gmail.com>
2025-05-28 19:31:14 +0900

    FizzBuzz問題の解答です

---
+++ .vscode-server/bin/848b80aeb52026648a8ff9f7c45a9b0a80641e2e/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="code"
+VERSION="1.100.2"
+COMMIT="848b80aeb52026648a8ff9f7c45a9b0a80641e2e"
+EXEC_NAME="code"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
